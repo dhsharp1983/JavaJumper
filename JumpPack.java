27db96e4c4e5452a7e01be2882276db021197879
David Sharp <sharpdavid83@gmail.com>
2023-08-05 15:59:37 +1000

    Commit First Version of Jump Pack and Player

--- JumpPack.java
+++ JumpPack.java
@@ -1,4 +1,65 @@
-public class JumpPack 
+// Class to create the Jump Pack object and hold its properties 
+// Author: David Sharp
+// Version: 1.0
+
+public class JumpPack
 {
-    
+    private int batteryLevel;
+    public final int MAX_BATTERY_LEVEL = 20;
+
+    // default constructor, is OK to use in production 
+    public JumpPack()
+    {
+        batteryLevel = 10;
+    }
+
+    // non-default constructor, unlikely to be needed 
+    public JumpPack(int batteryLevel)
+    {
+        this.batteryLevel = batteryLevel;
+    }
+
+    // charge battery if fuel cell found. If fuel cell found, battery cannot exceed max.
+    public void chargeBattery(int batteryPoints)
+    {
+        this.batteryLevel += batteryPoints;
+        if (this.batteryLevel > 20)
+        {
+            this.batteryLevel = MAX_BATTERY_LEVEL;
+        }
+    }
+
+    // deplete battery during play. If battery runs out, cannot go under 0.
+    public void depleteBattery(int batteryPoints)
+    {
+        this.batteryLevel -= batteryPoints;
+        if (this.batteryLevel < 0)
+        {
+            this.batteryLevel = 0;
+        }
+    }
+
+    // display method 
+    public String display()
+    {
+        return ("Jump Pack Battery Level: " + this.batteryLevel);
+    }
+
+    // Accessor method for Battery Level 
+    public int getBatteryLevel()
+    {
+        return this.batteryLevel;
+    }
+
+    // Accessor method for Max Battery, adding for consistency 
+    public int getMaxBatteryLevel()
+    {
+        return this.MAX_BATTERY_LEVEL;
+    }
+
+    // Mutator method for Battery Level 
+    public void setBatteryLevel(int batteryLevel)
+    {
+        this.batteryLevel = batteryLevel;
+    }
 }
--- Player.java
+++ Player.java
@@ -1,4 +1,68 @@
+// Player Class - for Player object 
+// Author: David Sharp
+// Version: 1.0
+
 public class Player
 {
-    
+    private String playerName;
+    private int onBuilding;
+    private int fuelCellsFound;
+
+    // default constructor
+    public Player()
+    {
+        this.playerName = "Unknown";
+        this.onBuilding = 1;
+        this.fuelCellsFound = 0;
+    }
+
+    // non-default constructor 
+    public Player(String name, int onBuilding, int fuelCellsFound)
+    {
+        this.playerName = name;
+        this.onBuilding = onBuilding;
+        this.fuelCellsFound = fuelCellsFound;
+    }
+
+    // display method 
+    public String display()
+    {
+        return ("Player Name: " + this.playerName + "; Player On Building: " + this.onBuilding + "; Player Fuel Cells Found: " + this.fuelCellsFound);
+    }
+
+    // retrieve how many fuel cells the player has found 
+    public int getFuelCellsFound()
+    {
+        return this.fuelCellsFound;
+    }
+
+    // retrieve which building the player is on 
+    public int getOnBuilding()
+    {
+        return this.onBuilding;
+    }
+
+    // retrieve the player's name 
+    public String getPlayerName()
+    {
+        return this.playerName;
+    }
+
+    // set how many fuel cells the player has found 
+    public void setFuelCellsFound(int fuelCellsFound)
+    {
+        this.fuelCellsFound = fuelCellsFound;
+    }
+
+    // set which building the player is on 
+    public void setOnBuilding(int onBuilding)
+    {
+        this.onBuilding = onBuilding;
+    }
+
+    // set the player's name 
+    public void setPlayerName(String name)
+    {
+        this.playerName = name;
+    }
 }
